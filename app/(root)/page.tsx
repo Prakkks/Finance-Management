915d70be96bf97ae7b56213be1615817471f5058
Prakkks <tprakrity@gmail.com>
2025-07-10 14:59:16 +0545

    Implemented routes and make homepage UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,28 @@
+import HeaderBox from '@/components/ui/HeaderBox'
+import RightSideBar from '@/components/ui/RightSideBar';
+import TotalBlncBox from '@/components/ui/TotalBlncBox';
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = { firstName: 'Prakriti', lastName: 'Thapa', email: 'tprakrity@gmail.com'};
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className='home-content'> 
+            <header className='home-header'>
+               <HeaderBox type='greeting' title='Welcome' user= { loggedIn?.firstName || 'Guest'}
+               subtext = 'Access and manage your account and transactions efficiently.'
+               />
+            
+            <TotalBlncBox  accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1256.43}  />
+            </header>
+            RECENT TRANSACTIONS 
+
+        </div>
+        <RightSideBar user= {loggedIn} transactions = {[]} banks= {[{currentBalance: '1230.45'},{currentBalance: '500'} ]} />
+    </section>
+
   )
 }
 
